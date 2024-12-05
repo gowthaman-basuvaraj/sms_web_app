20976b2e3cc8a62a9cb7cffdeeb8f5586e44785a
gowthaman <gowthaman@basuvaraj.com>
2024-12-05 16:06:37 +0530

    Add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./Component/Navbar";
 import { SocketProvider } from "./Component/SocketProvider";
 import { HandleAccess } from "./store/AccessHandle";
 import { useDispatch, useSelector } from "react-redux";
 import { setImageURL, setSelectedChat } from "./store/Store";
 import Chat from "./Component/Chat";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const dispatch = useDispatch();
-
   const { haveAccess } = useSelector((state) => state.auth);
+  const [loading, setLoading] = useState(true);
 
   const handleSelectChat = (chat, imageURL) => {
     dispatch(setSelectedChat(chat));
     dispatch(setImageURL(imageURL));
   };
+
+  const fetchAccessState = async () => {
+    await new Promise((r) => setTimeout(r, 500));
+    setLoading(false);
+  };
+  
+  useEffect(() => {
+    fetchAccessState();
+  }, []);
+
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-gray-800 text-white w-full">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <SocketProvider handleSelectChat={handleSelectChat}>
@@ -25,7 +44,10 @@ const App = () => {
           <div className="flex flex-grow">
             <Routes>
               {haveAccess ? (
-                <Route path="/" element={<Chat />} />
+                <>
+                  <Route path="/" element={<Chat />} />
+                  <Route path="*" element={<Navigate to="/" />} />
+                </>
               ) : (
                 <Route
                   path="*"
