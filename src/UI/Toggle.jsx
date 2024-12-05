29739d08a202ae680d2345ae5f3e9c09a232c0da
gowthaman <gowthaman@basuvaraj.com>
2024-12-05 13:47:01 +0530

    Mute Toggle functionality

--- src/UI/Toggle.jsx
+++ src/UI/Toggle.jsx
@@ -1,11 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
-import {updateMutePreference} from "../store/Store.js"
+import { updateMutePreference } from "../store/Store.js";
+import { useState } from "react";
 
 const Toggle = () => {
   const dispatch = useDispatch();
   const { selectedChat, user } = useSelector((state) => state.auth);
-  
+  const [muteState, setMuteState] = useState(selectedChat.mute);
+
   const handleToggle = () => {
+    setMuteState(!muteState);
     const userName = user.name;
     const sender = selectedChat.sender;
     const mute = !selectedChat.mute;
@@ -16,22 +19,22 @@ const Toggle = () => {
     <label className="flex items-center relative w-max cursor-pointer select-none">
       <input
         type="checkbox"
-        value={selectedChat.mute}
+        value={muteState}
         onChange={handleToggle}
         className={`appearance-none transition-colors cursor-pointer w-20 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 ${
-          selectedChat.mute ? "bg-green-500" : "bg-red-500"
+          muteState ? "bg-green-500" : "bg-red-500"
         }`}
       />
       <span
         className={`absolute font-medium text-[8px] uppercase ${
-          selectedChat.mute ? "right-8" : "right-1"
+          muteState ? "right-8" : "right-1"
         } text-white`}
       >
-        {selectedChat.mute ? "Muted" : "Not Muted"}
+        {muteState ? "Muted" : "Not Muted"}
       </span>
       <span
         className={`w-7 h-7 absolute rounded-full transform transition-transform bg-gray-200 ${
-          selectedChat.mute ? "translate-x-14" : ""
+          muteState ? "translate-x-14" : ""
         }`}
       />
     </label>
